8c36f324bcd933e90aca4f4bdc608322320d4f34
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-03-03 10:56:17 -0800

    Added images, js, html for landing page and clock. Counter working. Adding dates to dropdown. Need to add button, event listener, and js to handle the user input.

---
+++ script.js
@@ -0,0 +1,23 @@
+let countDownDate = new Date("Dec 13, 2023 13:13:13").getTime();
+
+let x = setInterval(function() {
+
+  let now = new Date().getTime();
+
+  let distance = countDownDate - now;
+
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("clock").innerHTML = 
+  days + " days <br>" 
+  + hours + " hours <br>"
+  + minutes + " minutes <br>" + seconds + " seconds ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("clock").innerHTML = "SHOW TIME!";
+  }
+}, 1000);
