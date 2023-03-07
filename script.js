ddcaba82f1eda4e275492ea3902fc14b1ee58960
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-03-06 16:21:17 -0800

    Updated the dates and times with correct format. Added pluralize function to change text based on time amount.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let myConcert = JSON.parse(localStorage.getItem("concertDate")) || [];
+function pluralize(word, count) {
+  if (count === 1) {
+    return word;
+  } else {
+    return word + "s";
+  }
+}
 
+let myTime = localStorage.getItem("selectedOption") || ["Dec 13, 2023 13:13:13"];
+let countDownDate = new Date(myTime).getTime();
 
-let countDownDate = new Date("Dec 13, 2023 13:13:13").getTime();
-
-let x = setInterval(function() {
+let timeLeft = setInterval(function() {
 
   let now = new Date().getTime();
 
@@ -14,21 +20,20 @@ let x = setInterval(function() {
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+  // Display time until concert day and pluralize if necessary
   document.getElementById("clock").innerHTML = 
-  days + " days <br>" 
-  + hours + " hours <br>"
-  + minutes + " minutes <br>" + seconds + " seconds ";
+  days + " " + pluralize("day", days) + "<br>" 
+  + hours + " " + pluralize("hour", hours) + "<br>"
+  + minutes + " " + pluralize("minute", minutes) + "<br>"
+  + seconds + " " + pluralize("second", seconds) + " ";
 
   if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("clock").innerHTML = "SHOW TIME!";
+    clearInterval(timeLeft);
+    document.getElementById("clock").innerHTML = "TODAY IS THE DAY!";
   }
 }, 1000);
 
-
-
-
-
+// Function to submit form and save selection
 const form = document.getElementById("date-select");
 const dropdown = document.getElementById("dropdown-content");
 
@@ -36,10 +41,11 @@ form.addEventListener("submit", function(event) {
   // Prevent the form from submitting and refreshing the page
   event.preventDefault();
  
-
   // Get selected option value
   const selectedValue = dropdown.value;
 
   // Save selected value to local storage
   localStorage.setItem("selectedOption", selectedValue);
 });
+
+
